0990cb156ebd4385695e1611e98bc4fe151a0cea
mnihussain <md.nasiribnehussain@gmail.com>
2022-07-19 22:52:39 -0400

    Initial Project

---
+++ src/test/java/com/geico/qa/test/auto/AutoTestClass.java
@@ -0,0 +1,13 @@
+package com.geico.qa.test.auto;
+
+import org.testng.annotations.Test;
+
+import com.geico.qa.base.BaseClass;
+
+public class AutoTestClass extends BaseClass{
+
+	@Test
+	public void getAAutoQuote() {
+		
+	}
+}
